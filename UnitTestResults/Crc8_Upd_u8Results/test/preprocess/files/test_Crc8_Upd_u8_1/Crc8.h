ae615af895bd61a4a3a1a9bc7682493e88a8e5ab
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-03-10 15:06:56 +0100

    rerun the tests and update gitignore and ci

--- UnitTestResults/Crc8_Upd_u8Results/test/preprocess/files/test_Crc8_Upd_u8_1/Crc8.h
+++
@@ -1,8 +0,0 @@
-// CEEDLING NOTICE: This generated file only to be consumed by CMock
-
-#ifndef _CRC8_H_ // Ceedling-generated include guard
-#define _CRC8_H_
-
-uint8_t Crc8_Calc_u8(const uint8_t *dataPtr, size_t dataLen);
-
-#endif // _CRC8_H_
