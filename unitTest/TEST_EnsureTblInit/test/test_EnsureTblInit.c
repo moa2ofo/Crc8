c53278a3e50fb45c858b6eaeb68d9f46cb7e90a1
Andrea Monti <131896606+moa2ofo@users.noreply.github.com>
2026-03-10 15:59:56 +0100

    [AM] unit test has been auto generated - EnsureTblInit

--- unitTest/TEST_EnsureTblInit/test/test_EnsureTblInit.c
+++ unitTest/TEST_EnsureTblInit/test/test_EnsureTblInit.c
@@ -1,14 +1,32 @@
 #include "unity.h"
 #include <EnsureTblInit.h>
-
 #include "mock_Crc8.h"
 #include "mock_Crc8_priv.h"
 
-void setUp(void) {
-}
-void tearDown(void) {
-}
 
-void test_EnsureTblInit(void) {
-  TEST_IGNORE_MESSAGE("Auto-generated stub test");
+
+
+void setUp(void)
+{
+}
+void tearDown(void)
+{
+}
+void test_InitFlg_false_triggers_BuildTbl_and_sets_flag(void)
+{
+    set_Crc8TblInitFlg_b(false);
+    
+    BuildTbl_Expect();
+    
+    EnsureTblInit();
+    
+    TEST_ASSERT_TRUE(get_Crc8TblInitFlg_b());
+}
+void test_InitFlg_true_does_not_trigger_BuildTbl(void)
+{
+    set_Crc8TblInitFlg_b(true);
+    
+    EnsureTblInit();
+    
+    TEST_ASSERT_TRUE(get_Crc8TblInitFlg_b());
 }
